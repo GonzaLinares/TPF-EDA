99d4470bc6b02cbff8f13971d13a68ae5c784e36
Agustin Gullino <agullino@itba.edu.ar>
2021-05-26 21:30:11 -0300

    asd

--- MerkleTree.h
+++ MerkleTree.h
@@ -1,11 +1,15 @@
 #pragma once
 
+/******************************************************************************
+* INCLUDE HEADER FILES
+******************************************************************************/
 #include <iostream>
 #include "Tx.h"
 #include "MerkleNode.h"
 
-//TODO: arreglar la prolijidad
-
+/******************************************************************************
+* CLASS TEMPLATE MERKLE TREE
+******************************************************************************/
 template <std::string(*hashFunc)(std::string&)> class MerkleTree
 {
 
