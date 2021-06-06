b7b9f77e7b913d197f473234f50e0cd633358ea4
gonzalo ezequiel linares <gonza_123456789101112@hotmail.com>
2021-06-06 00:09:52 -0300

    Merging

--- Node/Client/Client.cpp
+++ Node/Client/Client.cpp
@@ -44,7 +44,7 @@ int Client::getStatus()
 }
 
 
-
+/*
 bool Client::download(std::list<std::string>& buffer, const char* usrname, unsigned int tweetCount)
 {
 	if (this->status == IDLE)
@@ -88,7 +88,7 @@ bool Client::download(std::list<std::string>& buffer, const char* usrname, unsig
 				curl_easy_cleanup(this->curl);
 				curl_multi_cleanup(this->multiCurl);
 				json receivedJSON = json::parse(this->receivedData);
-				if (1/*procesamiento(receivedJSON, buffer)*/)
+				if (1procesamiento(receivedJSON, buffer))
 				{
 
 				}
@@ -100,7 +100,7 @@ bool Client::download(std::list<std::string>& buffer, const char* usrname, unsig
 	return 1;
 
 }
-
+*/
 
 
 static size_t recieveCallback(char* ptr, size_t size, size_t nmemb, void* userdata)
