d9ec55ad32c363fdc7f19068510eec99bee4caac
Agustin Gullino <agullino@itba.edu.ar>
2021-06-10 16:27:33 -0300

    asd

--- main.cpp
+++ main.cpp
@@ -25,14 +25,14 @@ int main(int argc, char** argv) {
 	boost::asio::io_context contexto;
 	NodeFactory nodes(contexto);
 
-	//NODOS PARA DEBUGGING, TODO: Sacar
-	nodes.createFullNode(std::to_string(25576));
-	nodes.createSPVNode(std::to_string(25578));
-	nodes.createFullNode(std::to_string(25582) ,std::string("blockchain_sample_0.json"));
-	
-	nodes.getNodes()[2]->addNeighbour("127.0.0.1:25576", std::string("Full"));
-	nodes.getNodes()[1]->addNeighbour("127.0.0.1:25582", std::string("Full"));
-	//******************************************************
+	////NODOS PARA DEBUGGING, TODO: Sacar
+	//nodes.createFullNode(std::to_string(25576));
+	//nodes.createSPVNode(std::to_string(25578));
+	//nodes.createFullNode(std::to_string(25582) ,std::string("blockchain_sample_0.json"));
+	//
+	//nodes.getNodes()[2]->addNeighbour("127.0.0.1:25576", std::string("Full"));
+	//nodes.getNodes()[1]->addNeighbour("127.0.0.1:25582", std::string("Full"));
+	////******************************************************
 	
 	while (menu.getState() == RUNNING) {
 
