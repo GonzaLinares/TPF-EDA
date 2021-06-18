1eb476f3dbaf0ef0346ec8fec2164abca87b81e7
Damian Sergi <dsergi@itba.edu.ar>
2021-06-18 11:11:06 -0300

    kdnfkanuhosidubiuVJBHDKBADUIFÑÑS<BPDAIUB

--- main.cpp
+++ main.cpp
@@ -13,6 +13,8 @@
 #include "MerkleTree.h"
 #include "Hashing.h"
 #include "Node/NodeFactory.h"
+#include "cryptopp850/dll.h"
+#include "cryptopp850/sha.h"
 
 using namespace std;
 
@@ -24,6 +26,8 @@ int main(int argc, char** argv) {
 	Gui menu;
 	boost::asio::io_context contexto;
 	NodeFactory nodes(contexto);
+
+	CryptoPP::SHA256;	//c'est le hash
 
 	////NODOS PARA DEBUGGING, TODO: Sacar
 	//nodes.createFullNode(std::to_string(25576));
