c502684cc086650e41a216634594f4606a54872b
Damian Sergi <dsergi@itba.edu.ar>
2021-06-18 14:16:19 -0300

    nuevo hash mamis

--- main.cpp
+++ main.cpp
@@ -14,10 +14,9 @@
 #include "Hashing.h"
 #include "Node/NodeFactory.h"
 
-#include <cryptopp/dll.h>
+
+#include <cryptopp/hex.h>
 #include <cryptopp/sha.h>
-#include <cryptopp/filters.h>
-#include <cryptopp/base64.h>
 
 using namespace std;
 
@@ -30,29 +29,13 @@ int main(int argc, char** argv) {
 	boost::asio::io_context contexto;
 	NodeFactory nodes(contexto);
 
+	////NODOS PARA DEBUGGING
+	nodes.createFullNode(std::to_string(25576));
+	nodes.createSPVNode(std::to_string(25578));
+	nodes.createFullNode(std::to_string(25582) ,std::string("blockchain_sample_0.json"));
 	
-	CryptoPP::SHA256 hash;	//c'est le hash
-	CryptoPP::byte digest[CryptoPP::SHA256::DIGESTSIZE];
-	std::string message = "abcdefghijklmnopqrstuvwxyz";
-
-	hash.CalculateDigest(digest, (CryptoPP::byte*)message.c_str(), message.length());
-
-	CryptoPP::HexEncoder encoder;
-	std::string output;
-	encoder.Attach(new CryptoPP::StringSink(output));
-	encoder.Put(digest, sizeof(digest));
-	encoder.MessageEnd();
-
-	std::cout << output << std::endl;
-
-
-	////NODOS PARA DEBUGGING, TODO: Sacar
-	//nodes.createFullNode(std::to_string(25576));
-	//nodes.createSPVNode(std::to_string(25578));
-	//nodes.createFullNode(std::to_string(25582) ,std::string("blockchain_sample_0.json"));
-	//
-	//nodes.getNodes()[2]->addNeighbour("127.0.0.1:25576", std::string("Full"));
-	//nodes.getNodes()[1]->addNeighbour("127.0.0.1:25582", std::string("Full"));
+	nodes.getNodes()[2]->addNeighbour("127.0.0.1:25576", std::string("Full"));
+	nodes.getNodes()[1]->addNeighbour("127.0.0.1:25582", std::string("Full"));
 	////******************************************************
 	
 	while (menu.getState() == RUNNING) {
