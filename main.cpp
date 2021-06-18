d5808401e7a4713a9704826d12db116e1e5a82f1
Damian Sergi <dsergi@itba.edu.ar>
2021-06-18 12:08:43 -0300

    ,lkjsbdvglavjbañbvñavbjñkabvñkcbvkñlbanjkfnñdklm´klmcd´skalmf

--- main.cpp
+++ main.cpp
@@ -13,8 +13,11 @@
 #include "MerkleTree.h"
 #include "Hashing.h"
 #include "Node/NodeFactory.h"
-#include "cryptopp850/dll.h"
-#include "cryptopp850/sha.h"
+
+#include <dll.h>
+#include <sha.h>
+#include <filters.h>
+#include <base64.h>
 
 using namespace std;
 
@@ -27,7 +30,21 @@ int main(int argc, char** argv) {
 	boost::asio::io_context contexto;
 	NodeFactory nodes(contexto);
 
-	CryptoPP::SHA256;	//c'est le hash
+	
+	CryptoPP::SHA256 hash;	//c'est le hash
+	CryptoPP::byte digest[CryptoPP::SHA256::DIGESTSIZE];
+	std::string message = "abcdefghijklmnopqrstuvwxyz";
+
+	hash.CalculateDigest(digest, (CryptoPP::byte*)message.c_str(), message.length());
+
+	CryptoPP::HexEncoder encoder;
+	std::string output;
+	encoder.Attach(new CryptoPP::StringSink(output));
+	encoder.Put(digest, sizeof(digest));
+	encoder.MessageEnd();
+
+	std::cout << output << std::endl;
+
 
 	////NODOS PARA DEBUGGING, TODO: Sacar
 	//nodes.createFullNode(std::to_string(25576));
